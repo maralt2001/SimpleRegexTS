ca91f0b2567df513936b669eb301a59f4a4dcf5d
Markus <markus.altendorfer@kabelmail.de>
2022-11-14 18:56:07 +0100

    new build app.js

--- build/app.js
+++ build/app.js
@@ -6,6 +6,5 @@ let check = (0, functions_1.rgxStringArray)(names, new RegExp('^[a-m]')).sort();
 console.log(check);
 let toCheck = (0, functions_1.rgxSingleString)("20221105", new RegExp('[^3-9]*'));
 console.log(toCheck);
-//check a email
 let isEmail = (0, functions_1.rgxCheckEmail)('mmll.test@gmail.de');
 console.log(isEmail);
